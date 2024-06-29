8e24cb890f63458656df3cc736d09c117038895c
MadeReihan <reihanmade@gmail.com>
2024-06-29 21:56:56 +0700

    implementasi permission camera

--- screens/ScanQrScreen.tsx
+++ screens/ScanQrScreen.tsx
@@ -1,8 +1,16 @@
-import React, {useState} from 'react';
-import {View, StyleSheet, Text, Linking, Alert} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {
+  View,
+  StyleSheet,
+  Text,
+  Linking,
+  Alert,
+  ToastAndroid,
+} from 'react-native';
 import QRCodeScanner from 'react-native-qrcode-scanner';
-import {ScanQrScreenNavigationProp, ScanQrScreenRouteProp} from './types.tsx';
+import {check, PERMISSIONS, request, RESULTS} from 'react-native-permissions';
 import {Button, useTheme} from 'react-native-paper';
+import {ScanQrScreenNavigationProp, ScanQrScreenRouteProp} from './types';
 
 function isValidURL(string: string) {
   const res = string.match(
@@ -26,6 +34,44 @@ function ScanQrScreen({navigation}: Props) {
   const [scannerKey, setScannerKey] = useState(0);
   const [scanning, setScanning] = useState(true);
   const [temuQR, setTemuQR] = useState(true);
+  const [hasCameraPermission, setHasCameraPermission] = useState(false);
+
+  useEffect(() => {
+    checkCameraPermission();
+  }, []);
+
+  const checkCameraPermission = async () => {
+    const result = await check(PERMISSIONS.ANDROID.CAMERA);
+    handlePermissionResult(result);
+  };
+
+  const requestCameraPermission = async () => {
+    const result = await request(PERMISSIONS.ANDROID.CAMERA);
+    handlePermissionResult(result);
+  };
+
+  const handlePermissionResult = result => {
+    switch (result) {
+      case RESULTS.UNAVAILABLE:
+        ToastAndroid.show(
+          'Fitur kamera tidak tersedia di perangkat ini',
+          ToastAndroid.LONG,
+        );
+        break;
+      case RESULTS.DENIED:
+        requestCameraPermission();
+        break;
+      case RESULTS.GRANTED:
+        setHasCameraPermission(true);
+        break;
+      case RESULTS.BLOCKED:
+        ToastAndroid.show(
+          'Izin kamera diblokir. Aktifkan dari pengaturan.',
+          ToastAndroid.LONG,
+        );
+        break;
+    }
+  };
 
   const onSuccess = (e: QRCodeScanResult) => {
     const scannedData = e.data;
@@ -47,12 +93,19 @@ function ScanQrScreen({navigation}: Props) {
 
   return (
     <View style={styles.container}>
-      {scanning ? (
-        <QRCodeScanner key={scannerKey} onRead={onSuccess} />
+      {hasCameraPermission ? (
+        scanning ? (
+          <QRCodeScanner key={scannerKey} onRead={onSuccess} />
+        ) : (
+          <View style={styles.pleaseWaitStyle}>
+            <Text style={styles.centerText}>Please wait...</Text>
+          </View>
+        )
       ) : (
-        <View style={styles.pleaseWaitStyle}>
-          <Text style={styles.centerText}>Please wait...</Text>
-        </View>
+        <Text style={styles.permissionText}>
+          Izin kamera diperlukan untuk memindai QR code. Harap izinkan akses
+          kamera.
+        </Text>
       )}
       {temuQR ? (
         <Text style={styles.tidakMenemukanQR}>Tidak menemukan QR</Text>
@@ -106,6 +159,11 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     padding: 20,
     color: '#000',
+  },
+  permissionText: {
+    textAlign: 'center',
+    color: '#000',
+    marginTop: 20,
   },
 });
 
