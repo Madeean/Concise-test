60b22d9bb156ca0981628aae08bb0f3a0a9dc859
MadeReihan <reihanmade@gmail.com>
2024-07-02 14:40:50 +0700

    update redux logger dan redux persist

--- redux/Store.ts
+++ redux/Store.ts
@@ -1,12 +1,28 @@
 import {combineReducers, configureStore} from '@reduxjs/toolkit';
 import historyReducer from './reducers/History.ts';
+import {persistReducer, persistStore} from 'redux-persist';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import logger from 'redux-logger';
 
 const rootReducer = combineReducers({
   history: historyReducer,
 });
 
+const configuration = {
+  key: 'root',
+  storage: AsyncStorage,
+  version: 1,
+};
+const persistedReducer = persistReducer(configuration, rootReducer);
+
 const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware => {
+    return getDefaultMiddleware({
+      serializableCheck: false,
+    }).concat(logger);
+  },
 });
 
 export default store;
+export const persistor = persistStore(store);
